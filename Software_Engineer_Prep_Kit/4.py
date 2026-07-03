cdea9d9bc7d45dc09d866a19efe27fdbbd7e93b6
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-07-03 18:13:36 +0200

    finished 4

--- Software_Engineer_Prep_Kit/4.py
+++ Software_Engineer_Prep_Kit/4.py
@@ -22,19 +22,22 @@ def isNonTrivialRotation(s1, s2):
     # then, walk along the indices in order, confirming they are all the same.
     # if one is ever wrong, false.
     # if index of s2 is >= len(s2), index in s2 wraps back to zero
-    try:
-        s2_idx = s2.index(s1[0])
-    except ValueError:
+    if s1 == s2:
         return False
-    if s2_idx == 0:
-        return False
-    for i in range(len(s1)):
-        if s1[i] != s2[s2_idx]:
-            return False
-        s2_idx += 1
-        if s2_idx == len(s2):
-            s2_idx = 0
-    return True
+    all_indices = []
+    for i in range(len(s2)):
+        if s2[i] == s1[0]:
+            all_indices = all_indices + [i]
+    for s2_idx in all_indices:
+        for i in range(len(s1)):
+            if s1[i] != s2[s2_idx]:
+                break
+            s2_idx += 1
+            if s2_idx == len(s2):
+                s2_idx = 0
+            if i == len(s1) - 1:
+                return True
+    return False
 
 if __name__ == '__main__':
     s1 = input()
